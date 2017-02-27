58e40c0ae9e05b18928a1c0b082a76dcb2f509e0
thereckshop <thereckshop@mac.com>
2017-02-27 14:49:31 -0800

    finished Handlesbars implementation and got basic server functionality

--- public/scripts/views/splashView.js
+++ public/scripts/views/splashView.js
@@ -1,6 +1,9 @@
 'use strict';
-var source   = $("#entry-template").html();
-var template = Handlebars.compile(source);
 
-var context = {title: "My New Post", body: "This is my first post!"};
-var html    = template(context);
+//THIS WAS USED TO TEST HB FUNCTIONALITY
+// var source   = $("#entry-template").html();
+// var template = Handlebars.compile(source);
+//
+// var context = {title: "My New Post", body: "This is my first post!"};
+// // return template(context);
+// $('#testHB').append(template(context))
--- server.js
+++ server.js
@@ -7,3 +7,11 @@ const express = require('express');
 
 //Make Express fucntionality work
 const app = express();
+
+//Designated Port for express
+const PORT = process.env.PORT || 4000;
+
+//Express
+app.get('/', (request, response) => response.sendFile('index.html', {root: './public'}));
+app.use(express.static('./public'));
+app.listen(PORT, () => console.log(`Sever has started on port ${PORT}!`));
