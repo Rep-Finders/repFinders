c121e5d6e73836c0cd77e1639668e8efa2f891a3
Steven Johnson <stevenjohnson86@gmail.com>
2017-02-28 12:58:13 -0800

    added basic routes

---
+++ public/scripts/models/routes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+page('/', splashController.init);
+
+page('/results', resultsController.init);
+
+page('/results/city', resultsView.viewCity);//these 3 or 4 func subject to change based on how resultsView is coded
+page('/results/county', resultsView.viewCounty);
+page('/results/state', resultsView.viewState);
+page('/results/federal', resultsView.viewFederal);
+
+page('/about', aboutView.init);
+
+page();
