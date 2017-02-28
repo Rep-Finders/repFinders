99c2af493ca1dfbeddd9729934e9266da05ef124
Austin Rogers <contactALRogers@gmail.com>
2017-02-28 12:04:04 -0800

    completed functionality for level filters

--- public/scripts/views/resultsView.js
+++ public/scripts/views/resultsView.js
@@ -6,7 +6,26 @@
 
   resultsView.index = function (){
     //render results?
-  }
+    $('.resultsNav').on('click', '.repDataButton', function(){
+      $('.repData').hide();
+      $('#' + $(this).data('section') + 'Results').fadeIn();
+    });
+    $('.resultsNav .repDataButton:first').click();
+  };
+
+  resultsView.index();
 
   module.resultsView = resultsView;
 })(window);
+//
+// let viewController = {};
+//
+// viewController.handleNav = function() {
+//   $('.mainNav').on('click', '.activeTab', function(){
+//     $('.tabContent').hide();
+//     $('#' + $(this).data('section') + 'Display').fadeIn();
+//   });
+//   $('.mainNav .activeTab:first').click();
+// };
+//
+// viewController.handleNav();
